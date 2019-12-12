706f91317f346c695ad8991014b509401e186295
Paniz Ahang <pahang@ucvts.org>
2019-12-11 22:15:07 -0500

    rectangle

--- js/shapes.js
+++ js/shapes.js
@@ -18,7 +18,8 @@ window.onload = function() {
     // there are six event listeners being added for the staff solutions. you'll have an
     // equivalent set of six event listeners for your solutions. the first one is done for you.
 
-    this.document.getElementById("hello").onclick = sayHello;
+    document.getElementById("hello").onclick = sayHello;
+    document.getElementById("rectangle").onclick = drawRectangle;
 }
 
 /*
@@ -54,7 +55,48 @@ const sayHello = function () {
  */
 
 const drawRectangle = function() {
-    // write your exercise 2 code here
+  const drawing1 = document.getElementById('student-canvas-2');
+   const ctx = drawing1.getContext('2d');
+   ctx.clearRect(0, 0, drawing1.width, drawing1.height);
+
+   var width = 0;
+   var height = 0;
+   var x = 0;
+   var y = 0;
+   do {
+     var width = prompt("Width: ")
+     var height = prompt("Height: ")
+     var x = prompt("X: ")
+     var y = prompt("Y: ")
+     if (width == null || height == null || x == null || y == null) {
+       break;
+     }
+     if (width > 1024 || width < 1) {
+       alert("Your width must be between 1 and 1024.")
+     }
+     else if (height > 512 || height < 1) {
+       alert("Your height must be between 1 and 512.")
+     }
+     else if (x < 1 || x > 1024) {
+       alert("Your x-coordinate must be between 1 and 1024.")
+     }
+     else if (y < 1 || y > 512) {
+       alert("Your y-coordinate must be between 1 and 512.")
+     }
+     else if (isNaN(width) || isNaN(height) || isNaN(x) || isNaN(y)) {
+       alert("One of your values is not a number.")
+     }
+     else if (Number(width) + Number(x) > 1024 || Number(height) + Number(y) > 512) {
+       alert("Your rectangle won't fit on the canvas.")
+     }
+   } while (width > 1024 || width < 1 || height > 512 || height < 1 || x < 1 || x > 1024 || y < 1 || y > 512 || isNaN(width) || isNaN(height) || isNaN(x) || isNaN(y) || Number(width) + Number(x) > 1024 || Number(height) + Number(y) > 512)
+
+   if (!(width == null) && !(height == null) && !(x == null) && !(y == null)) {
+     ctx.beginPath();
+     ctx.rect(x, y, width, height);
+     ctx.closePath();
+     ctx.stroke();
+   }
 };
 
 /*
